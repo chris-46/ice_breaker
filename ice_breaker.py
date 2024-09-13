1cf42d6b6b6bedff8ec74c37369a1a497d26d934
Christopher Tjondro <chris462@students.cs.ubc.ca>
2024-09-12 20:30:29 -0700

    Leverage OutputParsers to turn the output in to a pydantic dict, allowing us to take the ice_breaker results and display it in another format.

--- ice_breaker.py
+++ ice_breaker.py
@@ -6,6 +6,7 @@ from langchain.chains import LLMChain
 from langchain_ollama import ChatOllama
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parsers import summary_parser
 
 def ice_break_with(name: str) -> str:
     linkedin_username = linkedin_lookup_agent(name=name)
@@ -16,16 +17,19 @@ def ice_break_with(name: str) -> str:
     given the information {information} about a person I want you to create:
     1. A short summary
     2. two interesting facts about them
+
+    \n{format_instructions}
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"], template=summary_template,
+        partial_variables={"format_instructions": summary_parser.get_format_instructions()} # For things we know the format of
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     #llm = ChatOllama(model="llama3:latest")
 
-    chain = summary_prompt_template | llm | StrOutputParser()
+    chain = summary_prompt_template | llm | summary_parser
 
     res = chain.invoke(input={"information": linkedin_data})
 
